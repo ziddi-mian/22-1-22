f70caffbb881bb0234512525bf4cd93466a8666e
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-22 09:54:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -87,17 +87,13 @@ dt = new Date(2014,10,2);
 console.log(add_years(dt, 10).toString());
 
 
-
-
-
-
-
 // Write a JavaScript function to add specified weeks to a date.   
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_weeks(dt, 10).toString());
 // Output :
 // "Sun Jan 11 2015 00:00:00 GMT+0530 (India Standard Time)"
+
 
 
 function add_weeks(dt, n) 
