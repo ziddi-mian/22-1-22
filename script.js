b47afeea7369f57cacf68ba3c0caa8815e1ce3d7
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-22 09:57:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -114,11 +114,6 @@ console.log(add_weeks(dt, 10).toString());
 // "Wed Sep 02 2015 00:00:00 GMT+0530 (India Standard Time)"
 
 
-
-
-
-
-
 function add_months(dt, n) 
  {
 
@@ -129,6 +124,7 @@ dt = new Date();
 console.log(add_months(dt, 10).toString());  
 dt = new Date(2014,10,2);
 console.log(add_months(dt, 10).toString());
+
 
 // Write a JavaScript function to get time differences in minutes between two dates.   
 // Test Data :
