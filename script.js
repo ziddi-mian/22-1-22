7628317a768a7ea61be26421c2fb3b586b1e9ccd
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-22 09:56:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -106,18 +106,14 @@ dt = new Date(2014,10,2);
 console.log(add_weeks(dt, 10).toString());
 
 
-
-
-
-
-
-
 // Write a JavaScript function to add specified months to a date.   
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_months(dt, 10).toString());
 // Output :
 // "Wed Sep 02 2015 00:00:00 GMT+0530 (India Standard Time)"
+
+
 
 
 function add_months(dt, n) 
