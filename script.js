e8d6fa6100fe8d43a4ed84f7ec2e35d2578ee01d
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-22 09:51:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -36,9 +36,6 @@ console.log(daylights_savings(dt));
 // console.log(diff_to_GMT(dt));
 // "+05.500"
 
-
-
-
 function diff_to_GMT(dt) 
 { 
     return (-dt.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(dt.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(dt.getTimezoneOffset() / 60)) + '00';
@@ -48,6 +45,7 @@ dt = new Date();
 console.log(diff_to_GMT(dt)); 
 dt = new Date(1989, 10, 1); 
 console.log(diff_to_GMT(dt));
+
 
 
 
